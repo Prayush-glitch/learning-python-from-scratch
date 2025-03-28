8ba14dcf826c4fa2e1c448b7688165a14cd4f758
Prayush-glitch <digitalprayush.gmail.com>
2025-03-28 08:23:07 +0545

    adding math program

---
+++ area.py
@@ -0,0 +1,4 @@
+import math
+radius= float(input("enter any number for raidus"))
+area= math.pi*pow(radius,2)
+print(f"the area of circle is {area}cm")
---
+++ math.py
@@ -0,0 +1,4 @@
+import math
+radius=float(input("enter any number for radius:"))
+circumference= 2 *math.pi*radius
+print(f"the cricumference of circle is {round(circumference,2)}cm")
