ef4c1e76ec8bb28281475c7f2101b5be4a0b87bf
Prayush-glitch <digitalprayush.gmail.com>
2025-03-28 07:49:58 +0545

    adding new file

---
+++ coe.py
@@ -0,0 +1,15 @@
+friends=10
+#friends +=1
+#friends -=2
+#friends *=3(
+#remainder= friends % 3
+#print(remainder)
+#print(friends)
+x=3.56
+y=-6
+z=5
+#result= round(x)
+#result=abs(y)
+#result=max(x,y,z)
+result=min(x,y,z)
+print(result)
